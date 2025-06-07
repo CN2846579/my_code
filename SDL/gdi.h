db71f76a868dcdda117f1ecae53ccdf8211142c9
CN2846579 <2298652164@qq.com>
2025-06-07 15:39:41 +0800

    Add files via upload

---
+++ SDL/gdi.h
@@ -0,0 +1,18 @@
+#pragma once
+Uint32 _RGB(SDL_Surface* surface, short r, short g, short b) {
+	if (!surface) {
+		return 0; // Return 0 if surface is null
+	}
+	return  SDL_MapRGB(surface->format, r, g, b);
+}
+#ifndef RGB
+typedef unsigned long       DWORD;
+typedef int                 BOOL;
+typedef unsigned char       BYTE;
+typedef unsigned short      WORD;
+typedef DWORD   COLORREF;
+#define RGB(r,g,b)          ((COLORREF)(((BYTE)(r)|((WORD)((BYTE)(g))<<8))|(((DWORD)(BYTE)(b))<<16)))
+#endif
+Uint32 _RGB(short r, short g, short b) {
+	return RGB(b, g, r);
+}
